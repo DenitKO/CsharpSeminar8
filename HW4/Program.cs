6fb3b71adc12164da9da07b3ae7075e8669a0a75
DenitKO <motovdenis@mail.ru>
2022-09-10 13:13:52 +0300

    added finished home work 4

--- HW4/Program.cs
+++ HW4/Program.cs
@@ -5,3 +5,65 @@
 // 27(0,0,1) 90(0,1,1)
 // 26(1,0,1) 55(1,1,1)
 
+bool DoNotReapete(int[,,] someArray, int digit)
+{
+    bool repeat = false;
+    for (int i = 0; i < someArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < someArray.GetLength(1); j++)
+        {
+            for (int k = 0; k < someArray.GetLength(2); k++)
+            {
+                if (someArray[i,j,k] == digit)
+                {
+                    return repeat=true;
+                }
+            }
+            
+        }
+    }
+    return repeat;
+}
+
+void Fill3xArray(int[,,] array)
+{
+    int rnd = new Random().Next(0, 100);
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            int k = 0;
+            while (k < array.GetLength(2))
+            {
+                if (!DoNotReapete(array, rnd))
+                {
+                    array[i, j, k] = rnd;
+                    k++;
+                }
+                else                 
+                {
+                    rnd = new Random().Next(0, 100);
+                }
+            }
+        }
+    }
+}
+
+void Print3xArray(int[,,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                Console.Write($"{array[i, j, k]}({i},{j},{k}) ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
+
+int[,,] martix = new int[2, 2, 2];
+Fill3xArray(martix);
+Print3xArray(martix);
